96747d820789f94db2a47fadea34d3d7be200063
Noel Nyende <72074456+neo256-star@users.noreply.github.com>
2021-08-26 09:30:28 +0300

    Create mass-energy-equivalence01.cpp
    This is the first edition  and that is why it's so simple and a bit erroneous . I hope im not lazy and I change it

---
+++ mass-energy-equivalence01.cpp
@@ -0,0 +1,29 @@
+//
+//Created by Nsquared on 26/8/2021 (D/M/Y)
+//
+
+#include <iostream>
+
+const double Sol{299792458}; //speed of light in a vacuum (m/s)
+double mass; //kg
+double energy;
+
+double square(double n){
+	return n*n;
+}
+
+void calculate(){
+	while(cin){
+		if(cin == mass)
+			std::cout << mass = energy / square(Sol) << '\n';
+		if(cin == energy )
+			std::cout << energy = mass*square(Sol) << '\n';
+	}
+}
+int main(){
+	try{
+		calculate();
+	}catch(...){
+		cerr << "Error: maybe you inputted speed of light . if you did you shouldn't have it's a constant\n "
+	}
+}
